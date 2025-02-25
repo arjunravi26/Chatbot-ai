5554e299b264a5c6c5fce73346282ff27c67c677
Arjun Ravi <121337838+arjunravi26@users.noreply.github.com>
2025-02-25 23:55:07 +0530

    added dynamic chunking

--- src/chunking.py
+++ src/chunking.py
@@ -5,23 +5,33 @@ class Chunking:
     def __init__(self):
         self.nlp = spacy.load("en-core-web-sm")
         self.tokenizer = AutoTokenizer('bert-base-uncased')
-    def chunking(self,text,chunk_size=256,overlap=50):
-        chunks = []
-        current_length = 0
+
+    def dynamic_chunking(self,text, max_token=512, overlap=50):
         doc = self.nlp(text)
+        chunks = []
         current_chunk = []
+        token_length = 0
+        # overlap_text = ""
+
         for sent in doc.sents:
-            sent_length = len(self.tokenizer.tokenize(sent))
-            if current_length + sent_length <=chunk_size:
+            curr_length = len(self.tokenizer.tokenize(sent.text))
+
+            if curr_length + token_length <= max_token:
                 current_chunk.append(sent.text)
-                current_length += sent_length
+                token_length += curr_length
             else:
-                if self.chunks:
-                    prev_tokens = self.tokenizer.tokenize(chunks[-1])
-                    prev_tokens =  prev_tokens[-overlap]
-                    print(prev_tokens)
-                    overlap_text = ' '.join(prev_tokens)
-                    chunks.append(current_chunk)
-                    current_length = sent_length
-                    current_chunk = [sent.text]
-            return chunks
+                if current_chunk:
+                    chunks.append(' '.join(current_chunk))  # Save current chunk
+                    # prev_tokens = tokenizer.tokenize(' '.join(current_chunk))
+                    # overlap_tokens = prev_tokens[-overlap:]
+                    # overlap_text = tokenizer.convert_tokens_to_string(overlap_tokens)
+
+                # Start new chunk with overlap
+                # current_chunk = [overlap_text, sent.text] if overlap_text else [sent.text]
+                current_chunk = [sent.text]
+                token_length = len(self.tokenizer.tokenize(' '.join(current_chunk)))  # Update length
+
+        if current_chunk:
+            chunks.append(' '.join(current_chunk))  # Append last chunk
+
+        return chunks
